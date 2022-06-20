a9172d90fab58947922d796e3642b9414b96d91c
Yannki <Jankaji@inmail.sk>
2022-06-20 15:40:20 +0100

    Director & Studio class

--- src/main/java/Director.java
+++ src/main/java/Director.java
@@ -1,4 +1,41 @@
 package main.java;
 
+import java.util.Date;
+
 public class Director {
+    private String firstName;
+    private String lastName;
+    private Date DoB;
+
+    public Director(){}
+
+    public Director(String firstName, String lastName, Date DoB) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.DoB = DoB;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Date getDoB() {
+        return DoB;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDoB(Date doB) {
+        DoB = doB;
+    }
 }
--- src/main/java/Studio.java
+++ src/main/java/Studio.java
@@ -1,4 +1,29 @@
 package main.java;
 
 public class Studio {
+    private String name;
+    private String location;
+
+    public Studio(){}
+
+    public Studio(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
